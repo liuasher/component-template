3cc7c4c7529d824773001b98314eb1eb1f6e37bc
liuyaxiong <50769330+liuasher@users.noreply.github.com>
2021-03-21 17:41:21 +0800

    feat：【threejs】添加threejs

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -9,13 +9,14 @@
 export default {
     name: 'Home',
     mounted() {
+        // create
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
         const renderer = new THREE.WebGLRenderer();
+        // set & add
         renderer.setSize(window.innerWidth, window.innerHeight);
         document.querySelector('#threejs-container').appendChild(renderer.domElement);
-
+    
         const geometry = new THREE.BoxGeometry();
         const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
         const cube = new THREE.Mesh(geometry, material);
