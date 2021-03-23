52f8570ac5700b6a219cef18e35234535f50d1fe
yaxiong.liu@aqara.com <50769330+liuasher@users.noreply.github.com>
2021-03-23 20:41:08 +0800

    feat：【threejs】提交doc

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,42 +1,47 @@
 <template>
-    <div class="home" id="threejs-container">
-        
-    </div>
+  <div class="home" id="threejs-container"></div>
 </template>
 
 <script>
-
 export default {
-    name: 'Home',
-    mounted() {
-        // create
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer();
-        // set & add
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.querySelector('#threejs-container').appendChild(renderer.domElement);
-    
-        const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
+  name: "Home",
+  mounted() {
+    // create
 
-        camera.position.z = 5;
+    const renderer = new THREE.WebGLRenderer();
+    // set & add
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document
+      .querySelector("#threejs-container")
+      .appendChild(renderer.domElement);
 
-        const animate = () => {
-            requestAnimationFrame(animate);
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      1,
+      500
+    );
+    camera.position.set(0, 0, 100);
+    camera.lookAt(0, 0, 0);
 
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
+    const scene = new THREE.Scene();
 
-            renderer.render(scene, camera);
-        };
+    /**
+     *
+     */
+    const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    const points = [];
+    points.push(new THREE.Vector3(-10, 0, 0));
+    points.push(new THREE.Vector3(0, 10, 0));
+    points.push(new THREE.Vector3(10, 0, 0));
 
-        animate();
-    },
-    components: {
-       
-    },
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+    const line = new THREE.Line(geometry, material);
+
+    scene.add(line);
+    renderer.render(scene, camera);
+  },
+  components: {},
 };
 </script>
--- src/views/dashboard_02.vue
+++ src/views/dashboard_02.vue
@@ -1,30 +1,29 @@
 <template>
   <div class="dashboard-02">
     我是Dist中引入的dashboard
-    <Graph03/>
-    <Graph02/>
-    <Graph03/>
-    <Dashboard01/>
+    <Graph03 />
+    <Graph02 />
+    <Graph03 />
+    <Dashboard01 />
   </div>
 </template>
 
 <script>
-
-import { Graph02, Graph03, Dashboard01 } from '../dist/dashboard.common.js';
+import { Graph02, Graph03, Dashboard01 } from "../dist/dashboard.common.js";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-      Graph03,
-      Graph02,
-      Dashboard01
+    Graph03,
+    Graph02,
+    Dashboard01,
   },
 };
 </script>
 <style scoped lang="scss">
-@import  '../dist/dashboard.css';
-.dashboard-02{
-    display: flex;
-    flex-direction: row;
+@import "../dist/dashboard.css";
+.dashboard-02 {
+  display: flex;
+  flex-direction: row;
 }
 </style>
